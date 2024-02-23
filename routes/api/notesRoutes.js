13d54dacab62db9966c3b2981d9c14a19fa7b3d5
Ryan Gurley <gurleyryan@gmail.com>
2024-02-22 21:47:26 -0500

    wrote DELETE route and formatted

--- routes/api/notesRoutes.js
+++ routes/api/notesRoutes.js
@@ -4,30 +4,48 @@ const { readFromFile, readAndAppend, writeToFile } = require('../../helpers/fsUt
 
 // GET Route for a specific note
 notes.get('/', (req, res) =>
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
 // POST Route for a new note
 notes.post('/', (req, res) => {
     const { title, text } = req.body;
-  
+
     if (title && text) {
-      const newNote = {
-        title,
-        text,
-        id: uuidv4(),
-      };
-  
-      readAndAppend(newNote, './db/db.json');
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      res.json(response);
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+
+        readAndAppend(newNote, './db/db.json');
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
     } else {
-      res.json('Error in creating new note.');
+        res.json('Error in creating new note.');
+    }
+});
+
+// DELETE Route for a specific note
+notes.delete("/:id", async (req, res) => {
+    try {
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            const db = JSON.parse(data);
+            const index = db.findIndex((notes) =>
+                notes.id === req.params.id
+            );
+            db.splice(index, 1);
+            writeToFile('./db/db.json', db);
+        })
+        location.reload();
+        res.status(200).json({ status: 'success' });
+    } catch (err) {
+        res.status(500).json(err);
     }
 });
 
